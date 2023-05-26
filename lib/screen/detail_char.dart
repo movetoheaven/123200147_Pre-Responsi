cae083ac9b75e7d35f7e651defb0d9dceec8e776
Ichvan Rachmawan <ichvan.rachmawan@gmail.com>
2023-05-27 00:40:00 +0700

    newest

--- lib/screen/detail_char.dart
+++ lib/screen/detail_char.dart
@@ -29,40 +29,55 @@ class DetailChar extends StatelessWidget {
             return Text("No data");
           }else{
             DetailCharacters data = DetailCharacters.fromJson(snapshot.data);
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: Image.network('https://api.genshin.dev/characters/${detailChar}/gacha-splash'),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.network('https://api.genshin.dev/nations/${data.nation.toLowerCase()}/icon', width: 50, height: 50,),
-                    Image.network('https://api.genshin.dev/elements/${data.vision.toLowerCase()}/icon', width: 50, height: 50,),
-                    Text(data.name, style: TextStyle(
-                      color: Colors.black, fontSize: 30,
-                    ),),
-                  ],
-                ),
-                _stars(data.rarity),
-                Text(data.affiliation, style: TextStyle(color: Colors.black),),
-                SizedBox(height: 30,),
-                
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 30),
-                  child: Text(data.description, style: TextStyle(color: Colors.black), textAlign: TextAlign.justify,),
-                ),
-                SizedBox(height: 30,),
-                Row(
-                  children: [
-                    Image.network('https://api.genshin.dev/characters/${data.name}/talent-na',width: 80,height: 80),
-                    Expanded(child: Text(data.skillTalents[0].description, textAlign: TextAlign.justify,))
-                  ],
-                )
-
-              ],
+            return SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.network('https://api.genshin.dev/characters/${detailChar}/gacha-splash'),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.network('https://api.genshin.dev/nations/${data.nation.toLowerCase()}/icon', width: 50, height: 50,),
+                      Image.network('https://api.genshin.dev/elements/${data.vision.toLowerCase()}/icon', width: 50, height: 50,),
+                      Text(data.name, style: TextStyle(
+                        color: Colors.black, fontSize: 30,
+                      ),),
+                    ],
+                  ),
+                  _stars(data.rarity),
+                  Text(data.affiliation, style: TextStyle(color: Colors.black),),
+                  SizedBox(height: 30,),
+                  
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    child: Text(data.description, style: TextStyle(color: Colors.black), textAlign: TextAlign.justify,),
+                  ),
+                  SizedBox(height: 30,),
+                  Row(
+                    children: [
+                      Container(width: 80, height: 80,child: Expanded(child: Image.network('https://api.genshin.dev/characters/${data.name}/talent-${data.skillTalents}'))),
+                      Expanded(child: Text(data.skillTalents[0].description, textAlign: TextAlign.justify,))
+                    ],
+                  ),
+                  SizedBox(height: 30,),
+                  Row(
+                    children: [
+                      Container(width: 80, height: 80,child: Expanded(child: Image.network('https://api.genshin.dev/characters/${data.name}/talent-${data.skillTalents}'))),
+                      Expanded(child: Text(data.skillTalents[1].description, textAlign: TextAlign.justify,))
+                    ],
+                  ),
+                  SizedBox(height: 30,),
+                  Row(
+                    children: [
+                      Container(width: 80, height: 80,child: Expanded(child: Image.network('https://api.genshin.dev/characters/${data.name}/talent-${data.skillTalents}'))),
+                      Expanded(child: Text(data.skillTalents[2].description, textAlign: TextAlign.justify,))
+                    ],
+                  ),
+                ],
+              ),
             );
           }
         }
